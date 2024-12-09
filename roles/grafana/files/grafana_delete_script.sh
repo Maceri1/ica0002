66e0cbf64e7c492ed8ae42ee4dac97847f01be18
Matīss <maceri@taltech.ee>
2024-12-10 00:59:06 +0200

    Lab 14

--- roles/grafana/files/grafana_delete_script.sh
+++
@@ -1,8 +0,0 @@
-apt-get purge --auto-remove grafana
-sudo rm -rf /etc/grafana/          # Config files
-sudo rm -rf /var/lib/grafana/       # Data files, plugins, etc.
-sudo rm -rf /var/log/grafana/       # Log files
-sudo rm -rf /usr/share/grafana/     # Static files
-sudo userdel grafana
-sudo groupdel grafana
-
